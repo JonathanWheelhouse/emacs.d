ed2f0501e785030db51b5a097474fc2fee1b5236
Jonathan Wheelhouse <jonathan.wheelhouse@gmail.com>
2018-05-18 16:33:06 +1000

    Use Hunspell rather than Aspell
    Hunspell is more popular and emacs now requires an aspell version 0.60
    or greater.  There is no such precompiled version for Windows.

--- init.el
+++ init.el
@@ -330,9 +330,10 @@
   :diminish flyspell-mode
   :config
   (when (eq system-type 'windows-nt)
-    (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/"))
-  (setq ispell-program-name "aspell" ; use aspell instead of ispell
-        ispell-extra-args '("--sug-mode=ultra"))
+    (add-to-list 'exec-path "d:/software/hunspell-1.3.2-3-w32-bin/bin"))
+  (setq ispell-program-name "hunspell" ; use hunspell instead of ispell
+        ispell-extra-args '("--sug-mode=ultra"
+                            "-d en_AU"))
   (add-hook 'text-mode-hook #'flyspell-mode)
   (add-hook 'prog-mode-hook #'flyspell-prog-mode))
 
