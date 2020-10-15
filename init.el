12e06f8acc5b4e9dec782ced2263a3afa8c8ce09
Jonathan Wheelhouse <jonathan.wheelhouse@gmail.com>
2020-10-15 20:20:57 +1100

    Remove custom variables

--- init.el
+++ init.el
@@ -451,20 +451,5 @@
 (use-package yaml-mode
   :ensure t
   :mode "\\.json\\'")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (deadgrep yaml-mode diminish omnisharp request-deferred meson-mode solarized-theme ace-window ivy ggtags abbrev counsel swiper csharp-mode visual-regexp json-mode js2-mode which-key aggressive-indent flycheck imenu-anywhere zop-to-char company markdown-mode smex ido-ubiquitous rainbow-mode rainbow-delimiters move-text anzu multiple-cursors smartparens expand-region projectile magit avy material-theme use-package)))
- '(safe-local-variable-values (quote ((bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")))))
 
 ;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
